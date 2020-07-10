40b910dd43e87866998ff5a12161935f4c7493fa
Noraelk <67266165+Noraelk@users.noreply.github.com>
2020-07-10 16:18:46 +0200

    jour 12

--- EX-01/mini-site-routing.php
+++ EX-01/mini-site-routing.php
@@ -11,9 +11,10 @@
 <a href="http://localhost:8888/ISCC-2020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=3">Page2</a>
 <a href="http://localhost:8888/ISCC-2020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=connexion">Connexion</a>
 <?php
-session_start();
-if (session_status() == PHP_SESSION_ACTIVE)
+
+if ($_COOKIE['id']){
 echo '<a href="http://localhost:8888/ISCC-2020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=admin">Admin</a>';
+}
 ?>
 </nav>
 </header>
@@ -27,36 +28,41 @@ elseif($_GET['page']== 2){
  echo"Page 2 !";
  include ('Page1.php'); }
  
- elseif($_GET['page']=='connexion'){
- echo "Connexion !";
- include ('Connexion.php');
- }
- elseif($_GET['page']=='admin'){
-include('admin.php');
-echo "Admin !";
- }
 elseif($_GET['page']==3){
  echo "Page 3 !";
  include ('Page2.php');
-}
+
+}elseif ($_GET['page'] == 'connexion') {
+    echo "Connexion !";
+    include('Connexion.php');
+}elseif ($_GET['page'] == 'admin') {
+   
+    ?>
+    <form method="post" action="admin.php" enctype="multipart/form-data">
+<input type="texte" name="login" placeholder="Login"/>
+<input type="password" name="password" placeholder="Password"/>
+<input type="hidden" name="taille_maximale" value="2097152"/>
+<input name="userfile" type="file" accept="image/x-png,image/jpg,image/jpeg"/>
+<input type="submit" name="bouton" value="Charger le fichier"/>
+</form>
+<?php
+include('admin.php');
+    }
 ?>
 </h1>
 <p>
 <?php
-if(array_key_exists('id' ,$SESSION)==true){
-echo'Login: '.$_SESSION['id'].'.';};
- 
-$cleid = array_keys($_SESSION);
- 
-if(array_key_exists('id' ,$_SESSION)==false){
- if(empty($_COOKIE["$cleid[0]"])==false){
-$_COOKIE[$cleid[0]]=$_SESSION['login'];
-$_COOKIE[$_SESSION['id']]=$_SESSION['id'];
-}
-else{
- 
-}
-}
+       if (isset($_SESSION["id"])) {
+        echo 'Login: ' . $_SESSION["id"] . '.';
+    }elseif(isset($_COOKIE["id"])) {
+    $_SESSION['id']=$_POST['login'];
+    $SESSION['mdp']=$_POST['password'];
+        } 
+        else {
+            header('Location:http://localhost:8888/ISCC-2020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=connexion');
+        exit();
+        }
 ?>
+</P>
 </body>
 </html>
--- EX-01/securite.php
+++ EX-01/securite.php
@@ -5,22 +5,65 @@ session_start();
 echo $_POST['login'];
 echo '<br>';
 echo $_POST['password'];
-echo '<br>' ;
+echo '<br>';
 
-if ($_POST ['password']=='2020'){
-    $_SESSION['id']=$_POST['login'];
+function connect_to_database()
+{
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+  $databasename = "base-site-rooting";
 
-header('Location:http://localhost:8888/ISCC%20-%202020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=1');
+  try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    echo "Vous êtes connectés <br>";
+    return($pdo);
+  } catch (PDOException $e) {
+    echo "La connexion a échoué" . $e->getMessage();
+  }
 }
-elseif($_POST['password'] !='2020'){
-    echo 'Mauvais couple identifiant / mot de passe.<br>';
-    echo '<a href="http://localhost:8888/ISCC%20-%202020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=connexion">Connexion</a>';
+
+
+function login($pdo)
+{
+  try {
+    if (!empty($_POST['login']) && !empty($_POST['password'])) {
+
+      $login = $_POST['login'];
+      $password = $_POST['password'];
+      echo '1er if bon';
+     
+      $requete=$pdo->query("SELECT passwordd
+      FROM utilisateurs 
+      WHERE loginn='$login'");
+    $res=$requete->fetchAll();
+     
+
+      if ($res) {
+        
+        
+        if ($password == $res[0]['passwordd']) {
+          echo "Connection réussie : bon couple identifiant / mot de passe.";
+        } else {
+          echo "Mauvais couple identitifant / mot de passe.";
+        }
+      } else {
+        echo '<a href="http://localhost:8888/ISCC%20-%202020/ISCC-2020-J12/EX-01/mini-site-routing.php?page=connexion">Connexion</a>';
+      }
+    }
+  } catch (PDOException $e) {
+    echo "Login erreur" . $e->getMessage();
+  }
 }
+
+
+$pdo = connect_to_database();
+login($pdo);
 
 $cleid = array_keys($_SESSION);
-if(array_key_exists('id',$_SESSION)==true){
-  setcookie ($cleid[0],$_SESSION['id']);
+if (array_key_exists('id', $_SESSION) == true) {
+  setcookie($cleid[0], $_SESSION['id']);
 }
-
 ?>
